f5a90a3c7aa8008ab3359d808015792b71918a5a
Abilash Lathaharan <it20132682@my.sliit.lk>
2022-10-15 09:47:52 +0530

    doctor validation

--- backend/src/Pages/AddDoctors.js
+++ backend/src/Pages/AddDoctors.js
@@ -3,6 +3,7 @@ import { Button } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import Header from "../Header";
 import axios from "axios";
+import Col from "react-bootstrap/esm/Col";
 
 function AddDoctor() {
   const [name, setName] = useState("");
@@ -13,8 +14,59 @@ function AddDoctor() {
   const [bio, setBio] = useState("");
   const [image, setImage] = useState({ myFile: "" });
 
+   //error
+   const [error, setError] = useState(null);
+   const [loading, setLoading] = useState(false);
+   let [errors_name, seterrors_name] = useState("");
+   let [errors_age, seterrors_age] = useState("");
+   let [errors_specialization, seterrors_specialization] = useState("");
+   let [errors_dob, seterrors_dob] = useState("");
+   let [errors_number, seterrors_number] = useState("");
+   let [errors_bio, seterrors_bio] = useState("");
+
   function adddoctor() {
-    axios
+    
+    setError(null);
+    setLoading(true);
+
+    let errors = {};
+
+    //Form Validation
+    if (!name.trim()) {
+      errors.name = "Doctor's Name field required";
+      seterrors_name(errors.name);
+    }
+    if (!specialization.trim()) {
+      errors.specialization = "Specialization field required";
+      seterrors_specialization(errors.specialization);
+    }
+    if (!age.trim()) {
+      errors.age = "Age field required";
+      seterrors_age(errors.age);
+    }
+    if (!dob.trim()) {
+      errors.dob = "Date of birth field required";
+      seterrors_dob(errors.dob);
+    }
+    if (!number.trim()) {
+      errors.number = "Phone Number field required";
+      seterrors_number(errors.number);
+    }
+    if (!bio.trim()) {
+      errors.bio = "Bio field required";
+      seterrors_bio(errors.bio);
+    }
+    if (
+      name === "" ||
+      specialization === "" ||
+      age === "" ||
+      dob === "" ||
+      number === "" ||
+      bio === ""
+    ) {
+      setLoading(false);
+    } else {
+      axios
       .post("http://localhost:9999/doctormanagement/", {
         DoctorName: name,
         Specialization: specialization,
@@ -32,6 +84,7 @@ function AddDoctor() {
       .catch((err) => {
         console.log(err.data);
       });
+    }
   }
 
   const convertToBase64 = (file) => {
@@ -124,6 +177,7 @@ function AddDoctor() {
           </div>
           <div class="form-group">
             <label for="exampleFormControlInput1">Doctor Name</label>
+            <Col sm={12}>
             <input
               autoFocus
               autoComplete="off"
@@ -131,18 +185,33 @@ function AddDoctor() {
               id="exampleFormControlInput1"
               onChange={(e) => setName(e.target.value)}
             />
+              {errors_name && (
+                <span style={{ color: "red" }} className="errors">
+                  {errors_name}
+                </span>
+              )}
+            </Col>
+            
           </div>
           <div class="form-group">
             <label for="exampleFormControlInput1">Specialization</label>
+            <Col sm={12}>
             <input
               autoComplete="off"
               class="form-control"
               id="exampleFormControlInput1"
               onChange={(e) => setSpecialization(e.target.value)}
             />
+              {errors_specialization && (
+                <span style={{ color: "red" }} className="errors">
+                  {errors_specialization}
+                </span>
+              )}
+            </Col>
           </div>
           <div class="form-group">
             <label for="exampleFormControlInput1">Age</label>
+            <Col sm={12}>
             <input
               autoComplete="off"
               type="number"
@@ -150,27 +219,48 @@ function AddDoctor() {
               id="exampleFormControlInput1"
               onChange={(e) => setAge(e.target.value)}
             />
+              {errors_age && (
+                <span style={{ color: "red" }} className="errors">
+                  {errors_age}
+                </span>
+              )}
+            </Col>
           </div>
           <div class="form-group">
             <label for="exampleFormControlInput1">Date of Birth</label>
+            <Col sm={12}>
             <input
               type="date"
               class="form-control"
               id="exampleFormControlInput1"
               onChange={(e) => setDob(e.target.value)}
             />
+              {errors_dob && (
+                <span style={{ color: "red" }} className="errors">
+                  {errors_dob}
+                </span>
+              )}
+            </Col>
           </div>
           <div class="form-group">
             <label for="exampleFormControlInput1">Phone Number </label>
+            <Col sm={12}>
             <input
               autoComplete="off"
               class="form-control"
               id="exampleFormControlInput1"
               onChange={(e) => setNumber(e.target.value)}
             />
+              {errors_number && (
+                <span style={{ color: "red" }} className="errors">
+                  {errors_number}
+                </span>
+              )}
+            </Col>
           </div>
           <div class="form-group">
             <label for="exampleFormControlInput1">Bio</label>
+            <Col sm={12}>
             <input
               placeholder="Breif Discription about the Doctor's Service"
               autoComplete="off"
@@ -179,6 +269,12 @@ function AddDoctor() {
               style={{ height: "100px" }}
               onChange={(e) => setBio(e.target.value)}
             />
+              {errors_bio && (
+                <span style={{ color: "red" }} className="errors">
+                  {errors_bio}
+                </span>
+              )}
+            </Col>
           </div>
           <Button
             style={{ backgroundColor: "#307172", color: "white" }}
