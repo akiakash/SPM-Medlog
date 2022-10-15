a16d06a314361804bf0fa4d607d0d4ec6aea1c55
akash <akiagash12@gmail.com>
2022-10-15 13:22:24 +0530

    git validation

--- backend/src/Pages/AddAdmin.js
+++ backend/src/Pages/AddAdmin.js
@@ -5,6 +5,7 @@ import Header from "../Header";
 import axios from "axios";
 import { FormControl, MenuItem, Select } from "@mui/material";
 import { InputLabel } from "@mui/material";
+import Col from "react-bootstrap/esm/Col";
 
 function AddAdmin() {
   const [email, setEmail] = useState("");
@@ -16,38 +17,89 @@ function AddAdmin() {
   const [password, setPassword] = useState("");
   const [image, setImage] = useState({ myFile: "" });
 
+  //error
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  let [errors_name, seterrors_name] = useState("");
+  let [errors_email, seterrors_email] = useState("");
+  let [errors_role, seterrors_role] = useState("");
+  let [errors_age, seterrors_age] = useState("");
+  let [errors_dob, seterrors_dob] = useState("");
+  let [errors_number, seterrors_number] = useState("");
+
   const CreateUSer = (e) => {
-    axios
-      .post("http://localhost:9999/adminmanagement/signup", {
-        Name: name,
-        Email: email,
-        PhoneNumber: number,
-        Age: age,
-        Password: password,
-        Image: image.myFile,
-        Role: role,
-        DOB: dob,
-      })
-      .then((res) => {
-        console.log(res.data);
-        window.location.reload();
-        e.preventDefault();
-        setName("");
-        setNumber("");
-        setAge("");
-        setPassword("");
-        setImage("");
-        setEmail("");
-        setRole("");
-        setDob("");
+    setError(null);
+    setLoading(true);
 
-        alert("Successfully Registered");
+    let errors = {};
 
-        // Window.location.reload(true);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    //Form Validation
+    if (!name.trim()) {
+      errors.name = "Name field required";
+      seterrors_name(errors.name);
+    }
+    if (!email.trim()) {
+      errors.email = "Email field required";
+      seterrors_email(errors.email);
+    }
+    if (!number.trim()) {
+      errors.number = "Number  field required";
+      seterrors_number(errors.number);
+    }
+    if (!age.trim()) {
+      errors.age = "Age field required";
+      seterrors_name(errors.age);
+    }
+    if (!role.trim()) {
+      errors.role = "Role field required";
+      seterrors_role(errors.role);
+    }
+    if (!dob.trim()) {
+      errors.dob = "Dte of Birth field required";
+      seterrors_dob(errors.dob);
+    }
+    if (
+      name === "" ||
+      email === "" ||
+      number === "" ||
+      age === "" ||
+      role === "" ||
+      dob === ""
+    ) {
+      setLoading(false);
+    } else {
+      axios
+        .post("http://localhost:9999/adminmanagement/signup", {
+          Name: name,
+          Email: email,
+          PhoneNumber: number,
+          Age: age,
+          Password: password,
+          Image: image.myFile,
+          Role: role,
+          DOB: dob,
+        })
+        .then((res) => {
+          console.log(res.data);
+          window.location.reload();
+          e.preventDefault();
+          setName("");
+          setNumber("");
+          setAge("");
+          setPassword("");
+          setImage("");
+          setEmail("");
+          setRole("");
+          setDob("");
+
+          alert("Successfully Registered");
+
+          // Window.location.reload(true);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   };
 
   const convertToBase64 = (file) => {
@@ -140,19 +192,33 @@ function AddAdmin() {
           </div>
           <div class="form-group">
             <label for="exampleFormControlInput1">Admin Name</label>
-            <input
-              class="form-control"
-              id="exampleFormControlInput1"
-              onChange={(e) => setName(e.target.value)}
-            />
+            <Col sm={12}>
+              <input
+                class="form-control"
+                id="exampleFormControlInput1"
+                onChange={(e) => setName(e.target.value)}
+              />
+              {errors_name && (
+                <span style={{ color: "red" }} className="errors">
+                  {errors_name}
+                </span>
+              )}
+            </Col>
           </div>
           <div class="form-group">
             <label for="exampleFormControlInput1">Email</label>
-            <input
-              class="form-control"
-              id="exampleFormControlInput1"
-              onChange={(e) => setEmail(e.target.value)}
-            />
+            <Col sm={12}>
+              <input
+                class="form-control"
+                id="exampleFormControlInput1"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              {errors_email && (
+                <span style={{ color: "red" }} className="errors">
+                  {errors_email}
+                </span>
+              )}
+            </Col>
           </div>
 
           <div>
@@ -173,35 +239,63 @@ function AddAdmin() {
           </div>
           <div class="form-group">
             <label for="exampleFormControlInput1">Age</label>
-            <input
-              class="form-control"
-              id="exampleFormControlInput1"
-              onChange={(e) => setAge(e.target.value)}
-            />
+            <Col sm={12}>
+              <input
+                class="form-control"
+                id="exampleFormControlInput1"
+                onChange={(e) => setAge(e.target.value)}
+              />
+              {errors_name && (
+                <span style={{ color: "red" }} className="errors">
+                  {errors_name}
+                </span>
+              )}
+            </Col>
           </div>
           <div class="form-group">
             <label for="exampleFormControlInput1">Date of Birth</label>
-            <input
-              class="form-control"
-              id="exampleFormControlInput1"
-              onChange={(e) => setDob(e.target.value)}
-            />
+            <Col sm={12}>
+              <input
+                class="form-control"
+                id="exampleFormControlInput1"
+                onChange={(e) => setDob(e.target.value)}
+              />
+              {errors_dob && (
+                <span style={{ color: "red" }} className="errors">
+                  {errors_dob}
+                </span>
+              )}
+            </Col>
           </div>
           <div class="form-group">
             <label for="exampleFormControlInput1">Phone Number</label>
-            <input
-              class="form-control"
-              id="exampleFormControlInput1"
-              onChange={(e) => setNumber(e.target.value)}
-            />
+            <Col sm={12}>
+              <input
+                class="form-control"
+                id="exampleFormControlInput1"
+                onChange={(e) => setNumber(e.target.value)}
+              />
+              {errors_number && (
+                <span style={{ color: "red" }} className="errors">
+                  {errors_number}
+                </span>
+              )}
+            </Col>
           </div>
           <div class="form-group">
             <label for="exampleFormControlInput1">Password</label>
-            <input
-              class="form-control"
-              id="exampleFormControlInput1"
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <Col sm={12}>
+              <input
+                class="form-control"
+                id="exampleFormControlInput1"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {errors_number && (
+                <span style={{ color: "red" }} className="errors">
+                  {errors_number}
+                </span>
+              )}
+            </Col>
           </div>
 
           <Button
